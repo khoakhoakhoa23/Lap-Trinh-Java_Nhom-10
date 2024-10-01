4df79f9ed5a926b8036c23f17806b64db7e364af
khoakhoakhoa23 <tanhkhoa06@gmail.com>
2024-10-01 20:22:19 +0700

    them phuong thuc cho class tiemKiemSanPham

--- src/tiemKiemSanPham.java
+++ src/tiemKiemSanPham.java
@@ -1,9 +1,36 @@
 import java.util.List;
 
-public class tiemKiemSanPham extends heThongQuanLi{
-	public tiemKiemSanPham(List danhSachSanPham, List donHang, List taiKhoanKhachHang) {
-		super(danhSachSanPham, donHang, taiKhoanKhachHang);
-		// TODO Auto-generated constructor stub
+public class tiemKiemSanPham {
+	private String danhMuc;
+	private List<String> ketQuaTimKiem;
+	// phuong thuc get and set
+	
+	public String getDanhMuc() {
+		return danhMuc;
+	}
+	public void setDanhMuc(String danhMuc) {
+		this.danhMuc = danhMuc;
+	}
+	public List<String> getKetQuaTimKiem() {
+		return ketQuaTimKiem;
+	}
+	public void setKetQuaTimKiem(List<String> ketQuaTimKiem) {
+		this.ketQuaTimKiem = ketQuaTimKiem;
 	}
 
+	// tiem kiem san pham
+		public void timKiemSanPham(String tenSanPham) {
+			System.out.println("tiem kiem san pham" + tenSanPham);
+		}
+	// loc san pham
+		public void locSanPham(String theoLoai, double giaTien) {
+			// tu cho nhung danh sach
+			List<String> sanPham = List.of("Loai1 - 100.0", "Loai2 - 200.0", "Loai1 - 150.0");
+	        for (String sp : sanPham) {
+	            if (sp.contains(theoLoai) && sp.contains(String.valueOf(giaTien))) {
+	                ketQuaTimKiem.add(sp);
+	            }
+	        }
+		}
+	    		
 }
